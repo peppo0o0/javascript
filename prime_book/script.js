f911e8ca598016b1bc18e1d0d2689d906b7398dc
peppo0o0 <peppo0o01337@gmail.com>
2023-10-03 20:56:56 -0400

    using of variables

---
+++ prime_book/script.js
@@ -0,0 +1,6 @@
+/* Create the variable num and assign the value 123 to it. 
+Display the value of this variable to the screen using the alert function. */
+"use strict";   
+
+let num = 123;
+alert(num);
