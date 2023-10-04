a2eb2cb01c1cf4ce156d9f78b77f30dc45e964fe
peppo0o0 <peppo0o01337@gmail.com>
2023-10-04 01:25:23 -0400

    math operations on variables 3

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,9 +1,8 @@
-/* Create the variable c with the value 10 and variable d with the value 5. 
-Sum them up and assign the result to the variable result. Display the value of the variable result. */
+/* Create the variable a with the value 1, variable b with the value 2 and variable c with the value 3. Display their sum. */
 "use strict";   
 
-let c = 10;
-let d = 5;
+let a = 1;
+let b = 2;
+let c = 3;
 
-let result = c + d;
-alert(result);
+alert(a + b + c);
