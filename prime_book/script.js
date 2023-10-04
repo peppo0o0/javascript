8501e7a17d20c1f09274b845f7eb25d65d3e8fec
peppo0o0 <peppo0o01337@gmail.com>
2023-10-04 01:08:58 -0400

    math operations on numbers

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,9 +1,6 @@
-/* Create the variable a. Assign the value 10 to it, display it on the screen. 
-Then write the value 20 into it, display it on the screen. */
+/* Create the variable a. Write to it the sum of the numbers 1, 2 and 3. 
+Display the contents of the variable a, using the function alert. */
 "use strict";   
 
-let a = 10;
-alert(a);
-
-a = 20;
+let a = 1 + 2 + 3;
 alert(a);
