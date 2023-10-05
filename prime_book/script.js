ebdd70b2fbfb82d15b5a1962006ea2215af93e6b
peppo0o0 <peppo0o01337@gmail.com>
2023-10-04 20:26:20 -0400

    negative numbers 2

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,9 +1,6 @@
-// Write the number 1.5 into the variable a and the number 0.75 into the variable b. 
-// Find the sum of the values of these variables and display it on the screen.
+// Create the variable a, write some positive or negative number to it. 
+// Display this value with the opposite sign on the screen.
 "use strict";   
 
-let a = 1.5;
-let b = 0.75;
-
-let result = a + b;
-alert(result);
+let a = 7;
+alert(-a);
