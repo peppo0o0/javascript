d9dfcaee0aefaf29a22e961c1058c44adf5bfa67
peppo0o0 <peppo0o01337@gmail.com>
2023-10-04 21:22:48 -0400

    string concatenation 2

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,4 +1,8 @@
-// Raise the number 2 to the 10th power. Display the result on the screen.
+// Create the variable with the text 'java' and variable with the text 'script'. 
+// Using these variables and the string concatenation operation, display the string 'javascript' on the screen.
 "use strict";   
 
-alert(2**10);
+let a = 'java';
+let b = 'script';
+
+alert(a + b);
