cdb4f942756641e52a3f7c1742d0e397cb4a7464
peppo0o0 <peppo0o01337@gmail.com>
2023-10-09 22:16:55 -0400

    numbers extraction

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,8 +1,10 @@
-//Given a variable a with the value '10' and a variable b with the value '20'. 
-//Add the given variables as numbers.
+//Given a variable with the value '5px' and a variable with the value '6px'. 
+//Find the sum of pixels from the values of these variables and display it on the screen.
 
 'use strict';
-let a = '10';
-let b = '20';
 
-alert(Number(a) + Number(b));
+let num1 = parseInt("5px");
+let num2 = parseInt("6px");
+
+let res = num1 + num2;
+alert(res);
