1809e572c3b0daffbc2e706ff549e4f343d0448f
peppo0o0 <peppo0o01337@gmail.com>
2023-10-14 19:57:05 -0400

    last string character 1

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,6 +1,4 @@
-// Given the variable str with the string 'abcde' and the variable num with the character index. 
-// Display on the screen the character whose number is stored in the variable num.
-let str = "abcde";
-let num = 4;
+// Given a string. Display its last character on the screen.
+let str = "abcdedsadsadsajdsaasdixzkcnxzcinxcnxzcxzjiuL";
 
-alert(str[num]);
+alert(str[str.length - 1]);
