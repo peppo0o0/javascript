e752f4cc308ad89a6a3a54283b233c0ebacf6345
peppo0o0 <peppo0o01337@gmail.com>
2023-10-14 19:39:23 -0400

    string characters 2

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,6 +1,13 @@
-// Given the string 'abcde'. Referring to the individual characters of this string. 
-// print the character 'a', the character 'c', the character 'e'.
+// Given a variable with the string 'abcde'. Referring to the individual characters of this string.
+// write the characters of this string in reverse order into a new variable, that is, 'edcba'.
 let str = "abcde";
-alert(str[0]);
-alert(str[2]);
-alert(str[4]);
+
+let pos4 = str[4];
+let pos3 = str[3];
+let pos2 = str[2];
+let pos1 = str[1];
+let pos0 = str[0];
+
+let strReverse = pos4 + pos3 + pos2 + pos1 + pos0;
+
+alert(strReverse);
