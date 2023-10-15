14b7d2f7a7b6c4568ffeba664355399673d7583d
peppo0o0 <peppo0o01337@gmail.com>
2023-10-15 16:21:15 -0400

    accessing the digits of a number 2

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,5 +1,6 @@
-// The number given is 12345. Find the sum of the digits of this number.
+// The number given is 12345. Find the product of the digits of this number.
+
 let str = 12345;
 let strConvert = String(str);
 
-alert(Number(strConvert[0]) + Number(strConvert[1]) + Number(strConvert[2]) + Number(strConvert[3]) + Number(strConvert[4]));
+alert(strConvert[0] * strConvert[1] * strConvert[2] * strConvert[3] * strConvert[4]);
