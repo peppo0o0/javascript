24a75ed2513864b978a5c6aea6ccd2912c12e735
peppo0o0 <peppo0o01337@gmail.com>
2023-10-19 21:59:31 -0400

    finding mistakes practice 3

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,9 +1,9 @@
-// The code should find the sum of numbers:
+// The code should find the sum of the digits of the number:
 
-// let a = 1;
-// let b = 2;
-// console.log(a + b + c);
+// let num = '123';
+// let sum = num[0] + num[1] + num[2];
+// console.log(sum);
 
-let a = 1;
-let b = 2;
-console.log(a + b);
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
