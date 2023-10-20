d03871a66003850980f62ddb3ceaa8444b019c41
peppo0o0 <peppo0o01337@gmail.com>
2023-10-19 21:54:26 -0400

    finding mistakes practice 2

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,9 +1,9 @@
 // The code should find the sum of numbers:
 
-// let num1 = 1;
-// let num2 = 2;
-// console.log('Sum: ' + num1 + num2);
+// let a = 1;
+// let b = 2;
+// console.log(a + b + c);
 
-let num1 = 1;
-let num2 = 2;
-console.log(`Sum: ${num1 + num2}`);
+let a = 1;
+let b = 2;
+console.log(a + b);
