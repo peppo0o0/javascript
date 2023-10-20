c933a099b6567618af32e77b939c282d98964c0b
peppo0o0 <peppo0o01337@gmail.com>
2023-10-19 22:48:05 -0400

    finding mistakes practice 12

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,9 +1,13 @@
-// The code should output the last digit of the number:
+// The code should find the sum of the numbers:
 
-// let num = 123;
-// let str = String(num);
-// console.log(str[str.length]);
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + String(b);
 
-let num = 123;
-let str = String(num);
-console.log(str[str.length - 1]);
+// console.log(s);
+
+let a = '123';
+let b = '456';
+let s = Number(a) + Number(b);
+
+console.log(s);
