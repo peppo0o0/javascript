8e3c1a146cfa877aa41f352917c576a59ea8b9e3
peppo0o0 <peppo0o01337@gmail.com>
2023-10-22 22:17:04 -0400

    formulas practicum 1

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,3 +1,8 @@
-// Find the number of minutes in a year. 
+// Let the variable r with the radius of the circle be given. Using the appropriate formula.
+// find the area of the circle and write it to the variable s. 
+// Print the contents of this variable to the screen.
 
-alert((24 * 60) * 365);
+const PI = 3.14
+let r = prompt('Radius: ');
+let s = PI * r**2;
+alert(`Area: ${s}`);
