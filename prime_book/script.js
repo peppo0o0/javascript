b485a23d468c553caa18148e566b3dbe5f113933
peppo0o0 <peppo0o01337@gmail.com>
2023-10-23 21:07:04 -0400

    formulas practicum 4

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,8 +1,8 @@
 //  Let the variables a and b with the sides of a rectangle be given.
-//  Find the area of the rectangle and write it down in the variable s.
+//  Find the perimeter of the rectangle and write it to the variable p
 //  Print the contents of this variable to the screen.
 
 let a = prompt('height of a rectangle: ');
 let b = prompt('base of a rectangle: ');
-let s = (a * b);
-alert(`area of the rectangle: ${s}`)
+let p = ((2 * a) + (2 * b));
+alert(`the perimeter of the rectangle: ${p}`)
