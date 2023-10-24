581abda5571db768a5ee9c84d28c8ce6bb710cdc
peppo0o0 <peppo0o01337@gmail.com>
2023-10-23 20:59:17 -0400

    formulas practicum 3

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,7 +1,8 @@
-//  Let the variable a with the side of a square be given.
-//  Find the area of the square and write it to the variable s.
+//  Let the variables a and b with the sides of a rectangle be given.
+//  Find the area of the rectangle and write it down in the variable s.
 //  Print the contents of this variable to the screen.
 
-let a = prompt('side of a square: ');
-let s = (a * a);
-alert(`area of the square: ${s}`)
+let a = prompt('height of a rectangle: ');
+let b = prompt('base of a rectangle: ');
+let s = (a * b);
+alert(`area of the rectangle: ${s}`)
