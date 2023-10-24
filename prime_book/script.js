7355b8f1c2843e0ddc15e0a6d3a6148ea2d72c8c
peppo0o0 <peppo0o01337@gmail.com>
2023-10-23 21:20:17 -0400

    formulas practicum 6

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,6 +1,6 @@
-//  Let the variable tc with the temperature in degrees Celsius be given.
-//  Use the appropriate formula to convert this temperature to degrees Fahrenheit.
+//  Let the variable tf with the temperature in degrees Fahrenheit be given.
+//  Using the appropriate formula, to convert this temperature to degrees Celsius.
 
-let tc = prompt('Temperature (Cº): ');
-let tf = ((tc * 9/5) + 32);
-alert(`Temperature (Fº): ${tf}`)
+let tf = prompt('Temperature (Fº): ');
+let tc = ((tf - 32) * 5/9);
+alert(`Temperature (Cº): ${tc}`)
