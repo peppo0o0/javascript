6e76989b9587b9bbc6f1d57c9f6e8c2722e7b4e1
peppo0o0 <peppo0o01337@gmail.com>
2023-10-23 20:54:56 -0400

    formulas practicum 2

--- prime_book/script.js
+++ prime_book/script.js
@@ -1,8 +1,7 @@
-// Let the variable r with the radius of the circle be given. Using the appropriate formula.
-// find the area of the circle and write it to the variable s. 
-// Print the contents of this variable to the screen.
+//  Let the variable a with the side of a square be given.
+//  Find the area of the square and write it to the variable s.
+//  Print the contents of this variable to the screen.
 
-const PI = 3.14
-let r = prompt('Radius: ');
-let s = PI * r**2;
-alert(`Area: ${s}`);
+let a = prompt('side of a square: ');
+let s = (a * a);
+alert(`area of the square: ${s}`)
